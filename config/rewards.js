0b5208dd54c661262703a36c75d918ab2162ef5c
kaziemtiyez <kazitiktok60@gmail.com>
2026-08-15 16:48:26 +0300

    Add reward configuration for ads and referrals

---
+++ config/rewards.js
@@ -0,0 +1,21 @@
+export const rewardConfig = {
+  currency: "USD",
+
+  adsgram: {
+    rewardPerAd: 0.20,
+    dailyLimit: 15
+  },
+
+  monetag: {
+    rewardPerAd: 0.20,
+    dailyLimit: 10
+  },
+
+  referral: {
+    rewardPerReferral: 0.50
+  },
+
+  withdrawal: {
+    minimum: 10
+  }
+};
