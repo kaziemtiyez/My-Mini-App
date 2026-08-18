dead42caef2a18639310db056513967b2d622fa7
kaziemtiyez <your_email@gmail.com>
2026-08-18 15:37:41 +0300

    Fix frontend static serving

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(express.json());
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API Routes
